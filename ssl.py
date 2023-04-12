ea2f9d23366624565dd458046bdbf9101b33a3a3
DESKTOP-S7CN702\retro <woo4476@naver.com>
2023-04-12 20:35:21 +0900

    과제 파일 파이썬

--- ssl.py
+++ ssl.py
@@ -66,3 +66,73 @@ class LinkedList:
 	# size 메소드
 	def size(self):
 		return self.num_of_data 
+
+    # traverse_all 메소드 (head부터 tail까지 각 노드의 data 출력)
+	def traverse_all(self):
+		current_node = self.head.next
+		while current_node is not None:
+			print(f"({current_node.data}) -> ", end="")
+			current_node = current_node.next
+		print("null")
+
+
+    # insert_at 메소드 (position 위치에 new_data 노드 삽입)
+	def insert_at(self, position, new_data):
+        # position이 0 이하이면 error 문 출력 후 종료
+		if position <= 0:
+			print("Error: position이 0보다 커야합니다.")
+			return
+
+        # position이 리스트의 크기(원소 갯수)보다 크면 맨 마지막에 삽입
+		if position > self.num_of_data:
+			self.append(new_data)
+			return
+
+		new_node = Node(new_data)
+		current_node = self.head.next
+		before_node = self.head
+		current_position = 1
+
+		while current_position != position:
+			before_node = current_node
+			current_node = current_node.next
+			current_position += 1
+
+		new_node.next = current_node
+		before_node.next = new_node
+
+		self.num_of_data += 1
+
+# remove 메소드 (delete - 주어진 key 값을 갖는 노드 삭제, 데이터 개수 변경)
+	def remove(self, key):
+        # 데이터가 없는 경우 삭제할 노드도 없기 때문에 None 리턴
+		if self.num_of_data == 0:
+			print("해당하는 원소가 없습니다.")
+			return None
+
+        # key 값과 일치하는 노드를 찾기 위해 first 메소드 호출
+		self.first()
+
+        # 삭제할 노드가 맨 앞의 노드인 경우 (before와 head 변경)
+		if self.current.data == key:
+			self.head = self.head.next
+			self.current = self.head
+			self.before = self.head
+			self.num_of_data -= 1
+
+		else:
+			while self.current.next:
+                # 삭제할 노드 발견
+				if self.current.next.data == key:
+                    # 삭제할 노드가 tail인 경우 (tail과 before 변경)
+					if self.current.next is self.tail:
+						self.tail = self.current
+
+					self.current.next = self.current.next.next
+					self.num_of_data -= 1
+
+				else:
+					self.current = self.current.next
+					continue
+		
+		print(f"* {key}번째 원소(key)를 삭제합니다.")
